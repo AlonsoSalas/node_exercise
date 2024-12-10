9bc28bc82eed183462704903636237cc5b1b1a25
Alonso Salas <alosalasv@gmail.com>
2024-12-10 15:04:16 -0300

    feat(advice-api): main router to advice routes

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import adviceRoutes from './app/advice/routes';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('ADVICE API IS ONLINE!'));
+router.use('/advice', adviceRoutes);
 
 export default router;
